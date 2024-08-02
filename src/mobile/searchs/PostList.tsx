20891ff54b9b149f9d4c09e3b6a517dfdaec39d8
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-08-02 11:44:40 +0700

    Add product card

--- src/mobile/searchs/PostList.tsx
+++ src/mobile/searchs/PostList.tsx
@@ -1,12 +1,10 @@
 import useFilterState from '@mobile/filter_bds/hooks/useFilterState';
 import {
-  useQuery,
   useSuspenseQuery,
-  isServer,
+  queryOptions,
 } from '@tanstack/react-query';
 import { searchApi } from '@api/searchApi';
 import ProductCard from './ProductCard';
-import { homeApiOptions } from '@app/(home)/apis';
 
 export default function PostList() {
   const { buildFilterParams } = useFilterState();
@@ -14,19 +12,20 @@ export default function PostList() {
   filterParams.per_page = 12;
   console.log('filterParams', filterParams);
 
-  const { data } = useSuspenseQuery(homeApiOptions);
+  const { data } = useSuspenseQuery(
+    queryOptions({
+      queryKey: ['home', filterParams],
+      queryFn: async () => {
+        const response = await searchApi(filterParams);
 
-  // const clientData = useQuery({
-  //   queryKey: ['searchs-products', filterParams],
-  //   queryFn: async () => {
-  //     const response = await searchApi(filterParams);
-  //     return await response.json();
-  //   },
-  // });
+        return response.json();
+      },
+    })
+  );
 
   return (
-    <div className='w-full pt-4 relative mx-auto px-4 bg-white'>
-      <div className='text-secondary-600'>
+    <div className='w-full pt-4 relative mx-auto bg-white'>
+      <div className='text-secondary-600 px-4'>
         Có{' '}
         <span className='text-secondary-900'>
           {data?.pagination?.total_count}
--- src/mobile/searchs/ProductCard.tsx
+++ src/mobile/searchs/ProductCard.tsx
@@ -1,23 +1,105 @@
-import { Card } from 'konsta/react';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+
+import {
+  IoCloseOutline,
+  IoImage,
+  IoPlayCircleOutline,
+} from 'react-icons/io5';
+
+const styles: any = {
+  imagesCountWrapper: {
+    position: 'absolute',
+    bottom: 15,
+    left: 15,
+    height: 35,
+  },
+  imagesCount: {
+    backgroundColor: 'rgba(0,0,0,.3)',
+    display: 'flex',
+    borderRadius: 20,
+  },
+};
 
 export default function ProductCard({ product }: { product: any }) {
   return (
-    <Card outline key={product?.id}>
-      <div
-        className='ios:-mx-4 ios:-mt-4 h-48 p-4 flex items-end text-white ios:font-bold bg-cover bg-center material:rounded-xl mb-4 material:text-[22px]'
-        style={{
-          backgroundImage:
-            'url(https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg)',
-        }}
-      >
-        {product?.title}
+    <div className='m-4 overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md'>
+      <AspectRatio.Root ratio={16 / 9}>
+        <img
+          className='Image'
+          src={product?.featured_image_url}
+          alt={product?.name}
+        />
+
+        <div style={styles.imagesCountWrapper}>
+          <div
+            style={styles.imagesCount}
+            className='flex items-center justify-between px-2 py-1'
+          >
+            {/* <IoPlayCircleOutline
+              name='play-circle-outline'
+              size={20}
+              style={{ color: '#fff' }}
+            /> */}
+            <div
+              className='flex items-center justify-start'
+              style={{ marginLeft: 5 }}
+            >
+              <IoImage size={20} style={{ color: '#fff' }} />
+              <span style={{ color: '#fff', marginLeft: 5 }}>1</span>
+            </div>
+          </div>
+        </div>
+      </AspectRatio.Root>
+
+      <div className='p-4'>
+        <h3 className='mb-2 text-slate-600 font-bold'>
+          {product?.title}
+        </h3>
+
+        <div className='flex flex-row items-center justify-between'>
+          <div className='flex flex-col items-start justify-center'>
+            <span className='font-semibold text-xl'>
+              {product?.formatted_price}
+            </span>
+            <span className='text-slate-600 text-sm'>
+              {product?.formatted_price_per_m2}
+            </span>
+          </div>
+
+          <div className='flex flex-col items-start justify-center'>
+            <span className='font-semibold text-xl'>
+              {product?.formatted_area}
+            </span>
+            <span className='text-slate-600 text-sm'>
+              {product?.formatted_kt || '...'}
+            </span>
+          </div>
+
+          <div className='flex flex-col items-start justify-center'>
+            <div className='flex justify-center items-center'>
+              <img
+                src='https://spaces.chuannhadat.com/icons/bed_icon.svg'
+                width='16'
+                alt=''
+              />
+              <span className='ml-2'>{product?.bedrooms_count}</span>
+            </div>
+
+            <div className='flex justify-center items-center'>
+              <img
+                src='https://spaces.chuannhadat.com/icons/bath_icon.svg'
+                width='16'
+                alt=''
+              />
+              <span className='ml-2'>{product?.bathrooms_count}</span>
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-row items-center justify-between mt-3'>
+          <div className='text-slate-600'>Quận tân phú</div>
+          <div className='text-slate-600'>Hôm nay</div>
+        </div>
       </div>
-      <div className='text-gray mb-3'>Posted on January 21, 2021</div>
-      <p>
-        Quisque eget vestibulum nulla. Quisque quis dui quis ex
-        ultricies efficitur vitae non felis. Phasellus quis nibh
-        hendrerit...
-      </p>
-    </Card>
+    </div>
   );
 }
