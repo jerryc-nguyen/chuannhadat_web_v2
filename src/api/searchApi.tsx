db982d94c6f4f105d78f28d1a6c7680f3e89e5ab
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-08-09 18:11:30 +0700

    Build docker

--- src/api/searchApi.tsx
+++ src/api/searchApi.tsx
@@ -1,9 +1,10 @@
-import { apiFetch, baseUrl } from './base';
+import { apiFetch, apiBaseUrl } from './base';
+
 import { removeEmpty } from '@utils/hash';
 import queryString from 'query-string';
 
 export async function searchApi(params = {}): Promise<any> {
-  const path = `${baseUrl}/api/v1/searchs?${queryString.stringify(
+  const path = `${apiBaseUrl}/api/v1/searchs?${queryString.stringify(
     removeEmpty(params)
   )}`;
   return apiFetch(path, {
@@ -12,7 +13,7 @@ export async function searchApi(params = {}): Promise<any> {
 }
 
 export async function toParamsApi(params = {}): Promise<any> {
-  const path = `${baseUrl}/api/v1/searchs/to_params?${queryString.stringify(
+  const path = `${apiBaseUrl}/api/v1/searchs/to_params?${queryString.stringify(
     removeEmpty(params)
   )}`;
   return apiFetch(path, {
