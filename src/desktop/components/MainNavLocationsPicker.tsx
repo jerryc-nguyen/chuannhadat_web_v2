b0960f960b74718f59e183d5616c1d7afbadde48
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-09-22 23:10:05 +0700

    Finished main nav locations picker

--- src/desktop/components/MainNavLocationsPicker.tsx
+++ src/desktop/components/MainNavLocationsPicker.tsx
@@ -1,15 +1,33 @@
 import { Button } from "@components/ui/button";
 import Locations from "@desktop/product-filters/Locations";
+import useFilterState from "@mobile/filter_bds/hooks/useFilterState";
+import { useFilterLocations } from "@mobile/locations/hooks";
 import useModals from "@mobile/modals/hooks";
+import { FilterFieldName } from "@models";
 import { LuChevronsUpDown, LuMapPin } from "react-icons/lu";
 
+const ApplyButton = ({ closeModal }: { closeModal: IFunction }) => {
+  const { applySingleFilter } = useFilterState();
+
+  const applySelectLocations = () => {
+    applySingleFilter({ id: FilterFieldName.locations, text: 'Khu vực' })
+    closeModal();
+  }
+  return <Button onClick={() => applySelectLocations()}>Áp dụng</Button>
+}
+
 export default function MainNavLocationsPicker() {
-  const { openModal } = useModals()
+  const { selectedLocationText } = useFilterLocations();
+  const { openModal, closeModal } = useModals()
+  const { copyFilterStatesToLocal } = useFilterState();
+
   const showModalPickLocations = () => {
+    copyFilterStatesToLocal([FilterFieldName.locations]);
     openModal({
       name: 'ModalPickLocations',
       title: 'Chọn khu vực',
       content: <Locations />,
+      footer: <ApplyButton closeModal={closeModal} />,
       showAsDialog: true
     })
   }
@@ -20,8 +38,10 @@ export default function MainNavLocationsPicker() {
     className="w-[200px] justify-between rounded-full"
     onClick={() => showModalPickLocations()}
   >
-    <LuMapPin className="h-4 w-4 shrink-0 opacity-50" />
-    <span className='text-black'>Chọn khu vực</span>
+    <span className='flex items-center'>
+      <LuMapPin className="h-4 w-4 shrink-0 opacity-50 mr-1" />
+      <span className='text-black'>{selectedLocationText ?? 'Chọn khu vực'}</span>
+    </span>
     <LuChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
   </Button></>;
 }
