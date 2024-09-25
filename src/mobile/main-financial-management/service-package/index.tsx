be7d9a4ea3c94ce044802fb5b5052927c12b449d
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-09-25 11:58:50 +0700

    Fix layout mobile

--- src/mobile/main-financial-management/service-package/index.tsx
+++ src/mobile/main-financial-management/service-package/index.tsx
@@ -30,19 +30,20 @@ const ServicePackageView = () => {
     <div>
       <h3 className="mb-4 mt-8 text-xl font-bold">Mua gói dịch vụ</h3>
 
-      <div className=" mx-auto px-4 py-8">
-        <h1 className="mb-2 text-2xl font-bold">{currentServicePackage?.title}</h1>
-        <p className="text-gray-600 mb-0">{currentServicePackage?.sub_title}</p>
-        <a href="https://chuannhadat.com/huong-dan-tu-dong-lam-moi" target="_blank" rel="noopener noreferrer">
-          <Button variant="link" className='mb-8 text-[#007bff] pl-0'> {'>>'} Xem hướng dẫn cài đặt tự động làm mới tin giúp bạn tiết kiệm thời gian</Button>
-        </a>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-6">
-          {currentServicePackage &&
-            currentServicePackage?.plans.map((plan) => (
-              <ServiceCard key={plan.plan_id} plan={plan} />
-            ))}
-        </div>
+      <h1 className="mb-2 text-2xl font-bold">{currentServicePackage?.title}</h1>
+      <p className="text-gray-600 mb-0">{currentServicePackage?.sub_title}</p>
+
+      <a href="https://chuannhadat.com/huong-dan-tu-dong-lam-moi" target="_blank" rel="noopener noreferrer" className='text-[#007bff] my-4'>
+        Xem hướng dẫn cài đặt tự động làm mới tin giúp bạn tiết kiệm thời gian
+      </a>
+      <br />
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-6">
+        {currentServicePackage &&
+          currentServicePackage?.plans.map((plan) => (
+            <ServiceCard key={plan.plan_id} plan={plan} />
+          ))}
       </div>
+
     </div>
   );
 };
