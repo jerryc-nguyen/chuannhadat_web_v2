81564fb850a06f36d49a9b30456efac88804423e
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-09-29 21:39:42 +0700

    Render hover card in client side

--- src/desktop/home/CardAuthor.tsx
+++ src/desktop/home/CardAuthor.tsx
@@ -3,25 +3,32 @@ import { LuMoreHorizontal } from 'react-icons/lu';
 import useCardAuthors from './hooks/useCardAuthors';
 import { getInitialsName } from '@common/utils';
 import HoverCardAuthor from './HoverCardAuthor';
+import { isServer } from '@tanstack/react-query';
+import { ReactElement } from 'react';
 
+const SSRHoverComponent = ({ authorSlug, children }: { authorSlug: string, children: ReactElement }) => {
+  return <div data-slug={authorSlug}>{children}</div>
+}
 export default function CardAuthor({ product }: { product: A }) {
   const { getAuthorById } = useCardAuthors();
   const author = getAuthorById(product.user_id + '');
   const fullName = author?.full_name ? author.full_name : 'Loading';
+  const HoverComponent = (isServer || !author) ? SSRHoverComponent : HoverCardAuthor;
+  console.log('HoverComponent', isServer || !author, author)
   return (
     <div className="flex items-center justify-between p-4">
       <div className="flex items-center space-x-4">
-        <HoverCardAuthor authorSlug={author?.slug as string}>
+        <HoverComponent authorSlug={author?.slug as string}>
           <Avatar className="h-12 w-12">
             <AvatarImage src={author?.avatar_url} alt={fullName} />
             <AvatarFallback>{getInitialsName(fullName)}</AvatarFallback>
           </Avatar>
-        </HoverCardAuthor>
+        </HoverComponent>
 
         <div>
-          <HoverCardAuthor authorSlug={author?.slug as string}>
+          <HoverComponent authorSlug={author?.slug as string}>
             <p className="font-bold leading-none hover:underline">{fullName}</p>
-          </HoverCardAuthor>
+          </HoverComponent>
           <p className="mt-1 text-sm text-muted-foreground">
             {product?.formatted_publish_at} · {product?.short_location_name}
           </p>
