b3842cef9383743a6005ecfe659bae273d008710
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-11-11 21:38:42 +0700

    Add redirect tokens

---
+++ src/components/redirect-urls/constants.ts
@@ -0,0 +1,31 @@
+const oldCats: string[] = ["mua-ban-nha-dat",
+  "ban-can-ho-chung-cu",
+  "ban-nha-hem-nha-rieng",
+  "ban-biet-thu-lien-ke",
+  "ban-nha-mat-tien-nha-mat-pho-kinh-doanh-tot",
+  "ban-dat-nen-du-an",
+  "ban-dat-tho-cu-dat-nong-nghiep",
+  "ban-trang-trai-khu-nghi-duong",
+  "ban-nha-tro-phong-tro",
+  "ban-van-phong",
+  "ban-cua-hang-ki-ot",
+  "ban-kho-nha-xuong",
+  "ban-bat-dong-san-khac",
+  "nha-dat-cho-thue",
+  "cho-thue-can-ho-chung-cu",
+  "cho-thue-nha-hem-nha-rieng",
+  "cho-thue-biet-thu-lien-ke",
+  "cho-thue-nha-mat-tien-nha-mat-pho-kinh-doanh-tot",
+  "cho-thue-dat-nen-du-an",
+  "cho-thue-dat",
+  "cho-thue-trang-trai-khu-nghi-duong",
+  "cho-thue-nha-tro-phong-tro",
+  "cho-thue-van-phong",
+  "cho-thue-cua-hang-ki-ot",
+  "cho-thue-kho-nha-xuong",
+  "cho-thue-bat-dong-san-khac"
+]
+
+const oldPaths: string[] = ['/tin-ban/', '/tin-cho-thue/']
+
+export const redirectTokens = oldCats.concat(oldPaths)
