d159cbbbf90a357c12ee3749120a36af45046013
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-11-25 10:58:58 +0700

    Use figure card

--- src/desktop/dashboard/main-financial-management/components/BalanceInfo/index.tsx
+++ src/desktop/dashboard/main-financial-management/components/BalanceInfo/index.tsx
@@ -2,6 +2,8 @@ import useBalance from '@mobile/main-financial-management/hooks';
 import { IBalanceResponse } from '@mobile/main-financial-management/types';
 import { FC, useEffect, useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { CND_TEXT_COLORS } from '@common/constants';
+import FigureCard, { CardIcons } from '@desktop/dashboard/components/FigureCard';
 
 const BalanceInfo: FC<{ title: string }> = ({ title }) => {
   const { balanceInfo } = useBalance();
@@ -20,31 +22,26 @@ const BalanceInfo: FC<{ title: string }> = ({ title }) => {
     <div className="c-balanceInfo">
       <h3 className="my-4 text-xl font-bold">{title}</h3>
 
-      <div className="flex gap-4">
-        <Card className="flex-1">
-          <CardHeader className="mt-4 p-4 pb-0">
-            <CardTitle className="uppercase text-slate-400">Số dư tài khoản</CardTitle>
-          </CardHeader>
-          <CardContent className="p-4 text-xl text-[#007bff]">{balanceData.total}</CardContent>
-        </Card>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 mb-4">
+        <FigureCard
+          title="Số dư tài khoản"
+          contentClassName={CND_TEXT_COLORS.Success}
+          content={balanceData.total}
+          icon={CardIcons.Money}
+        />
 
-        <Card className="flex-1">
-          <CardHeader className="mt-4 p-4 pb-0">
-            <CardTitle className="uppercase text-slate-400">Tài khoản chính</CardTitle>
-          </CardHeader>
-          <CardContent className="gap-4 p-4 text-xl text-[#007bff]">
-            {balanceData.tk_chinh}
-          </CardContent>
-        </Card>
+        <FigureCard
+          title="Tài khoản chính"
+          content={balanceData.tk_chinh}
+          icon={CardIcons.Money}
+        />
 
-        <Card className="flex-1">
-          <CardHeader className="mt-4 p-4 pb-0">
-            <CardTitle className="uppercase text-slate-400">Tài khoản khuyến mãi</CardTitle>
-          </CardHeader>
-          <CardContent className="gap-4 p-4 text-xl text-[#fd7e14]">
-            {balanceData.tk_km}
-          </CardContent>
-        </Card>
+        <FigureCard
+          title="Tài khoản khuyến mãi"
+          content={balanceData.tk_km}
+          icon={CardIcons.Money}
+        />
+
       </div>
     </div>
   );
