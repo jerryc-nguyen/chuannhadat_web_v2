1d185f136f672d8fa4342e6972097a777aa4b790
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2024-12-04 11:42:53 +0700

    Update same style for other fields

--- src/desktop/dashboard/main-manage-post/manage-post/mobile/form-create.tsx
+++ src/desktop/dashboard/main-manage-post/manage-post/mobile/form-create.tsx
@@ -34,7 +34,7 @@ import ProductDescriptionForm, {
 } from '../components/form-components/product-description';
 import { PriceAutoComplete } from '../components/form-components/fields/price-autocomplete';
 import { RoundedOptionsNumberInput } from '../components/form-components/fields/rounded-options-number-input';
-import { roomsOptions } from '@mobile/filter_bds/constants';
+import { roomsOptionsForCreate, facadeOptionsForCreate } from '@mobile/filter_bds/constants';
 import { Button } from '@components/ui/button';
 
 /**
@@ -54,7 +54,7 @@ export const FormMobile: React.FC = () => {
       form.setValue('business_type', option.value);
     },
     options: businessTypeOptions,
-    btsTitle: 'Nhu cầu của bạn là (*)',
+    modalOptions: { title: 'Nhu cầu của bạn là (*)' },
     value: business_type,
   };
 
@@ -65,7 +65,7 @@ export const FormMobile: React.FC = () => {
       form.setValue('category_type', option.value);
     },
     options: categoryTypeOptions,
-    btsTitle: 'Loại BĐS (*)',
+    modalOptions: { title: 'Loại BĐS (*)' },
     value: category_type,
   };
 
@@ -87,12 +87,8 @@ export const FormMobile: React.FC = () => {
   const price_in_vnd = form.watch('price_in_vnd');
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onChangeFieldNumber = (field: ControllerRenderProps<any>, value: string) => {
-    // Regular expression to allow only numbers, with one optional comma or period, not at the beginning
-    const regex = /^(?![.,])\d+([.,]\d{0,})?$/;
-
-    if (regex.test(value) || value === '') {
-      field.onChange(value); // Update the value only if it matches the regex
-    }
+    console.log("field", field, value)
+    field.onChange(value); // Update the value only if it matches the regex
   };
 
   return (
@@ -185,7 +181,7 @@ export const FormMobile: React.FC = () => {
         />
         <ListItemBtsPicker
           options={phapLyTypeOptions}
-          btsTitle="Giấy tờ pháp lý"
+          modalOptions={{ title: 'Giấy tờ pháp lý' }}
           value={form.watch('phap_ly')}
           onSelect={(option) => {
             if (typeof option.value === 'string') form.setValue('phap_ly', option.value);
@@ -266,9 +262,10 @@ export const FormMobile: React.FC = () => {
                 }}
                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-expect-error
-                options={roomsOptions}
-                btsTitle="Phòng ngủ"
+                options={roomsOptionsForCreate}
                 value={field.value}
+                modalOptions={{ title: 'Phòng ngủ', maxHeightPercent: 0.7 }}
+                formattedValue={field.value ? `${field.value} PN` : ''}
                 footer={
                   <div className="p-4 gap-4 flex flex-col">
                     <Label>Số khác:</Label>
@@ -289,43 +286,79 @@ export const FormMobile: React.FC = () => {
           )}
         />
 
-        <div className="grid gap-4 md:grid-cols-2">
-          <FormField
-            control={form.control}
-            name="bedrooms_count"
-            render={({ field }) => (
-              <FormItem className={twMerge(styles.rowItemInput)}>
-                <FormLabel className="w-2/5">Số phòng ngủ</FormLabel>
-                <RoundedOptionsNumberInput
-                  {...field}
-                  className="relative"
-                  placeholder="Nhập số"
-                  onChange={(e) => onChangeFieldNumber(field, e.target.value)}
-                  maxLength={3}
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <FormField
+          control={form.control}
+          name="bathrooms_count"
+          render={({ field }) => (
+            <FormItem>
+              <ListItemBtsPicker
+                onSelect={(option: A) => {
+                  console.log('onSelect bathrooms_count', option);
+                  field.onChange(option.value);
+                }}
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                options={roomsOptionsForCreate}
+                value={field.value}
+                modalOptions={{ title: 'Phòng tắm', maxHeightPercent: 0.7 }}
+                formattedValue={field.value ? `${field.value} WC` : ''}
+                footer={
+                  <div className="p-4 gap-4 flex flex-col">
+                    <Label>Số khác:</Label>
+                    <RoundedOptionsNumberInput
+                      {...field}
+                      className="relative"
+                      placeholder="Nhập số"
+                      onChange={(e) => onChangeFieldNumber(field, e.target.value)}
+                      maxLength={3}
+                      hiddenSelect
+                    />
+                    <Button variant="default" className='w-full' onClick={closeModal}>OK</Button>
+                  </div>
+                }
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          <FormField
-            control={form.control}
-            name="bathrooms_count"
-            render={({ field }) => (
-              <FormItem className={twMerge(styles.rowItemInput)}>
-                <FormLabel className="w-2/5">Số phòng tắm</FormLabel>
-                <RoundedOptionsNumberInput
-                  {...field}
-                  className="relative"
-                  placeholder="Nhập số"
-                  onChange={(e) => onChangeFieldNumber(field, e.target.value)}
-                  maxLength={3}
-                />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <FormField
+          control={form.control}
+          name="facade"
+          render={({ field }) => (
+            <FormItem>
+              <ListItemBtsPicker
+                onSelect={(option: A) => {
+                  console.log('onSelect facade', option);
+                  field.onChange(option.value);
+                }}
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                options={facadeOptionsForCreate}
+                value={field.value}
+                modalOptions={{ title: 'Mặt tiền', maxHeightPercent: 0.7 }}
+                formattedValue={field.value ? `${field.value} m` : ''}
+                footer={
+                  <div className="p-4 gap-4 flex flex-col">
+                    <Label>Số khác:</Label>
+                    <RoundedOptionsNumberInput
+                      {...field}
+                      className="relative"
+                      placeholder="Nhập số"
+                      onChange={(e) => onChangeFieldNumber(field, e.target.value)}
+                      maxLength={3}
+                      hiddenSelect
+                    />
+                    <Button variant="default" className='w-full' onClick={closeModal}>OK</Button>
+                  </div>
+                }
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
+        {/* <div className="grid gap-4 md:grid-cols-2">
           <FormField
             control={form.control}
             name="facade"
@@ -377,9 +410,9 @@ export const FormMobile: React.FC = () => {
               </FormItem>
             )}
           />
-        </div>
+        </div> */}
 
-        <div className="grid gap-4 md:grid-cols-2">
+        {/* <div className="grid gap-4 md:grid-cols-2">
           <FormField
             control={form.control}
             name="floors_count"
@@ -397,11 +430,11 @@ export const FormMobile: React.FC = () => {
               </FormItem>
             )}
           />
-        </div>
+        </div> */}
 
         <ListItemBtsPicker
           options={directionOptions}
-          btsTitle="Hướng nhà/ đất"
+          modalOptions={{ title: 'Hướng nhà/ đất' }}
           value={form.watch('entrance_direction')}
           onSelect={(option) => {
             if (typeof option.value === 'string') form.setValue('entrance_direction', option.value);
@@ -410,7 +443,7 @@ export const FormMobile: React.FC = () => {
 
         <ListItemBtsPicker
           options={directionOptions}
-          btsTitle="Hướng ban công"
+          modalOptions={{ title: 'Hướng ban công' }}
           value={form.watch('view_direction')}
           onSelect={(option) => {
             if (typeof option.value === 'string') form.setValue('view_direction', option.value);
@@ -419,14 +452,14 @@ export const FormMobile: React.FC = () => {
 
         <ListItemBtsPicker
           options={furnitureTypeOptions}
-          btsTitle="Nội thất"
+          modalOptions={{ title: 'Nội thất' }}
           value={form.watch('furniture')}
           onSelect={(option) => {
             if (typeof option.value === 'string') form.setValue('furniture', option.value);
           }}
           dividers={false}
         />
-        <TestComponents />
+        {/* <TestComponents /> */}
       </List>
     </div>
   );
--- src/mobile/bts-pickers/ListItemBtsPicker.tsx
+++ src/mobile/bts-pickers/ListItemBtsPicker.tsx
@@ -2,20 +2,21 @@ import ListItem from "@components/konsta/ListItem";
 import useModals from "@mobile/modals/hooks";
 
 import { OptionForSelect } from "@models";
-import { directionsOptions } from "@mobile/filter_bds/constants";
 import ListCheckOptions from "@mobile/ui/ListCheckOptions";
+import { Modal } from "@mobile/modals/states/types";
 
 type IListItemBtsPickerProps = {
   options: Array<OptionForSelect>,
   onSelect?: (option: OptionForSelect) => void,
   value?: string,
-  btsTitle?: string,
   closeAfterSelect?: boolean
   dividers?: boolean
-  footer?: React.ReactNode
+  footer?: React.ReactNode,
+  modalOptions?: Modal,
+  formattedValue?: string
 }
 
-export default function ListItemBtsPicker({ onSelect, value, options, btsTitle, closeAfterSelect, dividers, footer }: IListItemBtsPickerProps) {
+export default function ListItemBtsPicker({ onSelect, value, options, closeAfterSelect, dividers, footer, modalOptions, formattedValue }: IListItemBtsPickerProps) {
   const { openModal, closeModal } = useModals();
   const selectedOption = options.find((item: OptionForSelect) => (item.value + '') == value)
 
@@ -32,16 +33,16 @@ export default function ListItemBtsPicker({ onSelect, value, options, btsTitle, 
   return (
     <ListItem
       link
-      title={btsTitle}
+      title={modalOptions?.title}
       onClick={() => {
         openModal({
-          name: `ListItemBtsPicker_${btsTitle}`,
-          title: btsTitle,
-          content: <ListCheckOptions selectedOption={selectedOption} onSelect={onLocalSelect} options={options} footer={footer}/>,
-          supportPushState: false
+          name: `ListItemBtsPicker_${modalOptions?.title}`,
+          title: modalOptions?.title,
+          content: <ListCheckOptions selectedOption={selectedOption} onSelect={onLocalSelect} options={options} footer={footer} />,
+          ...(modalOptions || {})
         });
       }}
-      after={selectedOption?.text}
+      after={formattedValue || selectedOption?.text}
       dividers={dividers}
     />
 
