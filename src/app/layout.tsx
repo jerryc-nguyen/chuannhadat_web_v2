10d0b21cebbc5ff47d25f485f38528434bea4fab
Nguyễn Văn Dũng <onebilliontimes@gmail.com>
2025-01-05 23:11:17 +0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const vietnam = Be_Vietnam_Pro({
   weight: ['300', '400', '700', '600', '500', '800', '900'],
 });
 
+// GOOD to know: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#unsupported-metadata
 export const metadata: Metadata = createMetadata({
   title: 'Đăng tin nhà đất, bất động sản chuyên nghiệp | Chuẩn Nhà Đất',
   description:
@@ -29,8 +30,88 @@ export const metadata: Metadata = createMetadata({
     siteName: 'chuannhadat.com',
     url: getServerSideURL(),
   },
-  manifest: "/manifest.json",
-  themeColor: "#ffffff",
+  manifest: '/manifest.json',
+  themeColor: '#ffffff',
+
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        url: '/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/android-icon-192x192.png',
+      },
+    ],
+    apple: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        url: '/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        url: '/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        url: '/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        url: '/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        url: '/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        url: '/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        url: '/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        url: '/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/apple-icon-180x180.png',
+      },
+    ]
+  },
+  other: {
+    'msapplication-TileColor': '#ffffff',
+    'msapplication-TileImage': '/ms-icon-144x144.png',
+  },
 });
 
 export const viewport: Viewport = {
@@ -57,22 +138,6 @@ export default function RootLayout({
         <link rel="canonical" href={`${getServerSideURL()}`}></link>
         <meta charSet="utf-8" />
         <meta content="IE=edge" http-equiv="X-UA-Compatible" />
-
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
       </Head>
       {/* ---------------- */}
       <body
