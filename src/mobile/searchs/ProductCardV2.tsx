a9d15d0220327c3692192d49999d5b20287e6195
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-01-19 12:42:52 +0700

    Prevent show top loader

--- src/mobile/searchs/ProductCardV2.tsx
+++ src/mobile/searchs/ProductCardV2.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import * as AspectRatio from '@radix-ui/react-aspect-ratio';
-import { IoImage } from 'react-icons/io5';
-import useResizeImage from '@hooks/useResizeImage';
 import { IProduct } from './type';
 import useModals from '@mobile/modals/hooks';
 import PostDetailMobile from '../post-detail/PostDetailMobile';
-import Image from 'next/image';
 import AuthorInfo from '@mobile/post-detail/components/AuthorInfo';
 import Link from 'next/link';
-import ButtonSave from '@desktop/home/components/ButtonSave';
-import LoadingProductCard from '@desktop/home/components/LoadingProductCard';
 import BedRoomIcon from '@assets/icons/badroom-icon';
 import BadRoomIcon from '@assets/icons/bedroom-icon';
 import CardAuthor from '@desktop/home/components/CardAuthor';
 import CardImageCarousel from '@desktop/home/components/CardImageCarousel/CardImageCarousel';
-
-const styles: A = {
-  imagesCountWrapper: {
-    position: 'absolute',
-    bottom: 15,
-    left: 15,
-    height: 35,
-  },
-  imagesCount: {
-    backgroundColor: 'rgba(0,0,0,.3)',
-    display: 'flex',
-    borderRadius: 20,
-  },
-};
 
 export const ProductDetailTitleBts = ({ product }: { product: A }) => {
   return (
@@ -57,11 +37,10 @@ export const ProductDetailTitleBts = ({ product }: { product: A }) => {
   );
 };
 export default function ProductCard({ product }: { product: IProduct }) {
-  const { buildThumbnailUrl } = useResizeImage();
   const { openModal } = useModals();
 
   const showDetailPostModal = (e: A) => {
-    e.preventDefault();
+
     openModal({
       name: product.title,
       title: <ProductDetailTitleBts product={product} />,
@@ -70,8 +49,14 @@ export default function ProductCard({ product }: { product: IProduct }) {
       footer: <AuthorInfo />,
       headerHeight: 74.59,
       footerHeight: 74.59,
-      pushToPath: `/post/${product.slug}`,
+      supportPushState: false
     });
+
+    e.preventDefault();
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+
+    return false
   };
 
   return (
@@ -83,13 +68,13 @@ export default function ProductCard({ product }: { product: IProduct }) {
       <div className="p-4">
         <div className="w-full text-secondary">{product.bus_cat_type}</div>
 
-        <Link
+        <a
           onClick={showDetailPostModal}
           href={`/post/${product.slug}`}
           className="mb-2 cursor-pointer font-bold text-secondary c-ads_color"
         >
           {product?.title}{' '}
-        </Link>
+        </a>
       </div>
       <div className="flex w-full justify-between px-4 pb-4">
         <div className="flex flex-col gap-y-1 text-sm">
