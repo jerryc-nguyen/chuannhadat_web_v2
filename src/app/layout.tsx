11ed7281a7bfd395cf51e49ec5a224e0ef355493
Nguyễn Văn Dũng <onebilliontimes@gmail.com>
2025-04-17 23:34:51 +0700

    add hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './index.scss';
 import ProviderWrapper from './provider-wrapper';
 import NextTopLoader from 'nextjs-toploader';
+import Script from 'next/script';
 
 const vietnam = Be_Vietnam_Pro({
   subsets: ['vietnamese'],
@@ -151,6 +152,18 @@ export default function RootLayout({
       >
         <NextTopLoader />
         <ProviderWrapper>{children}</ProviderWrapper>
+        <Script id="hotjar" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6375646,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </body>
     </html>
   );
