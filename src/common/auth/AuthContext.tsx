fafdc46147a4318c8fa39541339abe80a256a2c9
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-04-24 22:35:52 +0700

    Fix lỗi CK không auto

--- src/common/auth/AuthContext.tsx
+++ src/common/auth/AuthContext.tsx
@@ -256,7 +256,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   };
 
   // Calculate bankTransferNote 
-  const bankTransferNote = currentUser?.id?.toString() || '';
+  const bankTransferNote = "cnd" + currentUser?.id?.toString() || '';
 
   const value = {
     isAuthenticated,
