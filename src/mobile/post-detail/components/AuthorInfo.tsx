e30352925081c077e7d4df39b87b49f0deec541d
OptimusPrime2001 <trungbts3@gmail.com>
2025-06-08 02:26:54 +0700

    Update ESLint rules

--- src/mobile/post-detail/components/AuthorInfo.tsx
+++ src/mobile/post-detail/components/AuthorInfo.tsx
@@ -17,14 +17,13 @@ const AuthorInfo: React.FC = () => {
   const handleAuthorClick = () => {
     window.location.href = `/profile/${author?.slug}`
   }
-
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center justify-between" onClick={handleAuthorClick}>
         <div className="flex items-center">
           <img src={`/profile/${author?.slug}`} className="aspect-square h-12 w-12 rounded-full" alt="" />
           <div className='ml-2'>
-            <Link href={author?.slug as string} className="font-bold text-lg leading-none">{author?.full_name}</Link>
+            <Link href={`/profile/${author?.slug || ''}`} className="font-bold text-lg leading-none">{author?.full_name}</Link>
             <p className='text-sm text-secondary mt-1'>Đã đăng {author?.posts_count} tin</p>
           </div>
         </div>
