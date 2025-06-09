42fe0de48206234e6611a22d47d926a007e82a30
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-06-09 22:23:34 +0700

    Hide modal detail when click author

--- src/views/post-detail/components/author-post.tsx
+++ src/views/post-detail/components/author-post.tsx
@@ -3,18 +3,20 @@ import ButtonPhone from '@components/button-phone';
 import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar';
 import { Skeleton } from '@components/ui/skeleton';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+
 import React, { useMemo } from 'react';
 import { AuthorPostProps } from '../type';
 import PostsBySameAuthor from './posts-by-same-author';
+import { openModalDetail } from '@views/post-detail/states/modalPostDetailAtoms';
+import { useSetAtom } from 'jotai';
 
-const AuthorPost: React.FC<AuthorPostProps> = ( { data, className } ) => {
+const AuthorPost: React.FC<AuthorPostProps> = ({ data, className }) => {
+  const setOpenDetailModal = useSetAtom(openModalDetail);
   const dataAuthor = data?.author
-  const badgesCount = useMemo( () => {
-    return ( dataAuthor?.formatted_badges || [] ).length;
-  }, [] );
+  const badgesCount = useMemo(() => {
+    return (dataAuthor?.formatted_badges || []).length;
+  }, []);
 
-  const router = useRouter();
   const loadingAuthor = () => {
     return (
       <div className="author-post h-fit flex-1 rounded-md border bg-white p-6">
@@ -27,9 +29,9 @@ const AuthorPost: React.FC<AuthorPostProps> = ( { data, className } ) => {
           </div>
         </div>
         <div className="my-8 flex flex-col gap-y-2">
-          {new Array( 4 ).fill( 0 ).map( ( _item, index ) => (
-            <Skeleton key={genKey( index )} className="h-5 w-full" />
-          ) )}
+          {new Array(4).fill(0).map((_item, index) => (
+            <Skeleton key={genKey(index)} className="h-5 w-full" />
+          ))}
         </div>
         <div className="flex flex-col gap-y-2">
           <Skeleton className="h-[36px] w-full" />
@@ -38,25 +40,35 @@ const AuthorPost: React.FC<AuthorPostProps> = ( { data, className } ) => {
       </div>
     );
   };
+
+  const handleAuthorClick = () => {
+    setOpenDetailModal(false)
+  };
+
   return (
-    <div className={cn( 'author-post z-3 sticky top-0 h-fit min-w-[350px] flex-1', className )}>
+    <div className={cn('author-post z-3 sticky top-0 h-fit min-w-[350px] flex-1', className)}>
       {!dataAuthor ? (
         loadingAuthor()
       ) : (
         <div className="rounded-lg border bg-white p-4">
           <h3 className="text-lg font-semibold">Liên hệ:</h3>
           <div className="my-4 flex items-center gap-x-3">
-            <Avatar
-              onClick={() => router.push( `/profile/${data.author.slug}` )}
-              className="h-[50px] w-[50px] cursor-pointer"
+            <Link
+              href={`/profile/${dataAuthor.slug}`}
+              onClick={() => handleAuthorClick()}
             >
-              <AvatarImage src={dataAuthor?.avatar_url} />
-              <AvatarFallback>{getInitialsName( dataAuthor?.full_name as string )}</AvatarFallback>
-            </Avatar>
+              <Avatar
+                className="h-[50px] w-[50px] cursor-pointer"
+              >
+                <AvatarImage src={dataAuthor?.avatar_url} />
+                <AvatarFallback>{getInitialsName(dataAuthor?.full_name as string)}</AvatarFallback>
+              </Avatar>
+            </Link>
             <div>
               <Link
                 href={`/profile/${dataAuthor.slug}`}
                 className="cursor-pointer font-bold hover:underline"
+                onClick={() => handleAuthorClick()}
               >
                 {dataAuthor?.full_name || dataAuthor?.phone}
               </Link>
