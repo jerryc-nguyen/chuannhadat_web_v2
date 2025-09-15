6ecd02ffe6f8c3bb7c89551cbe7986ff99b65e90
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-09-15 21:25:11 +0700

    Improve update new email

--- src/app/dashboard/_components/AccountDetail/api/profile.ts
+++ src/app/dashboard/_components/AccountDetail/api/profile.ts
@@ -1,29 +1,46 @@
 import axiosInstance from '@common/api/axiosInstance';
 import { API_ROUTES } from '@common/router';
-import { IResponseData } from '@common/types';
+import { IResponseData, IUser } from '@common/types';
+import { IModalUpdatePassoword, IModalUpdateProfile } from '../types';
 
 export const profileApi = {
-  getMyProfile: async (): Promise<IResponseData<any>> => {
+  getMyProfile: async (): Promise<IResponseData<IUser>> => {
     return axiosInstance.get(API_ROUTES.PROFILES.GET_MY_PROFILE);
   },
 
-  updateEmail: async (email: string): Promise<any> => {
+  updateMyProfile: async (data: IModalUpdateProfile): Promise<IResponseData<IUser>> => {
+    return axiosInstance.put(API_ROUTES.PROFILES.UPDATE_MY_PROFILE, data);
+  },
+
+  updateMyAvatar: async (data: FormData): Promise<IResponseData<{ avatar_url: string }>> => {
+    return axiosInstance.post(API_ROUTES.PROFILES.UPDATE_AVATAR, data, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    });
+  },
+
+  updateMyPassword: async (data: IModalUpdatePassoword): Promise<IResponseData<{ success: boolean }>> => {
+    return axiosInstance.post(API_ROUTES.PROFILES.UPDATE_PASSWORD, data);
+  },
+
+  updateEmail: async (email: string): Promise<IResponseData<{ success: boolean }>> => {
     return axiosInstance.post(`${API_ROUTES.PROFILES.UPDATE_EMAIL}`, {
       email: email,
     });
   },
 
-  confirmEmail: async (confirm_email_token: string): Promise<any> => {
+  confirmEmail: async (confirm_email_token: string): Promise<IResponseData<{ success: boolean }>> => {
     return axiosInstance.post(`${API_ROUTES.PROFILES.CONFIRM_EMAIL}`, {
       confirm_email_token: confirm_email_token,
     });
   },
 
-  updateMyPhone: async (data: { phone: string }) => {
+  updateMyPhone: async (data: { phone: string }): Promise<IResponseData<{ success: boolean }>> => {
     return axiosInstance.post(API_ROUTES.PROFILES.UPDATE_PHONE, data);
   },
 
-  checkDeposit: async (last_credit_id: number): Promise<IResponseData<any>> => {
+  checkDeposit: async (last_credit_id: number): Promise<IResponseData<{ last_credit_id: number }>> => {
     return axiosInstance.post(API_ROUTES.PROFILES.CHECK_DEPOSIT, {
       last_credit_id: last_credit_id || undefined,
     });
