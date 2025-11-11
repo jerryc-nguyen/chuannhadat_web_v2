006aa5a66502ba20a8bfdfba54deab5438a78fb8
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-11-11 23:58:18 +0700

    Improve price builder + simplify posts query params

--- src/app/dashboard/_components/PostManagement/manage-posts/ListPostsV2/apis/product-api.ts
+++ src/app/dashboard/_components/PostManagement/manage-posts/ListPostsV2/apis/product-api.ts
@@ -1,33 +1,11 @@
 import { API_ROUTES } from '@common/router';
 import axiosInstance from '@common/api/axiosInstance';
-import { ProductQuery } from '../schemas/ProductQuerySchema';
 import { SetUpAutoRefreshProductInput, ShowOnFrontEndProductInput, UpVipProductInput } from '../schemas/UpVipProductInputSchema';
-
+import { removeEmpty } from '@common/utils';
 const ProductApiService = {
-  Filter: async (data: ProductQuery) => {
-    const params = new URLSearchParams();
-    data.business_type && params.append('business_type', data.business_type);
-    data.category_type && params.append('category_type', data.category_type);
-    data.city_id && params.append('city_id', data.city_id);
-    data.district_id && params.append('district_id', data.district_id);
-    data.ward_id && params.append('ward_id', data.ward_id);
-    data.street_id && params.append('street_id', data.street_id);
-    data.project_id && params.append('project_id', data.project_id);
-    data.floors_count && params.append('floors_count', data.floors_count);
-    data.furnitures && params.append('furnitures', data.furnitures);
-    data.mat_tien && params.append('mat_tien', data.mat_tien);
-    data.selected_ids && params.append('selected_ids', data.selected_ids);
-    data.sort_by && params.append('sort_by', data.sort_by);
-    data.sort_direction && params.append('sort_direction', data.sort_direction);
-    data.page && params.append('page', data.page.toString());
-    data.per_page && params.append('per_page', data.per_page.toString());
-
-    data.visibility && params.append("visibility", data.visibility);
-    data.search_by && params.append("search_by", data.search_by);
-    data.search_value && params.append("search_value", data.search_value);
-    data.filter_chips && params.append("filter_chips", data.filter_chips);
-    params.append("aggs_for", 'manage_posts');
-    const response = await axiosInstance.get(`${API_ROUTES.MANAGE_PRODUCTS.END_POINT}?${params.toString()}`);
+  Filter: async (data: Record<string, any>) => {
+    const queryData = removeEmpty(data);
+    const response = await axiosInstance.get(`${API_ROUTES.MANAGE_PRODUCTS.END_POINT}?${new URLSearchParams(queryData).toString()}`);
 
     // Our axios instance already returns response.data (payload object).
     // Keep returning the payload so controllers can read { data, pagination }.
