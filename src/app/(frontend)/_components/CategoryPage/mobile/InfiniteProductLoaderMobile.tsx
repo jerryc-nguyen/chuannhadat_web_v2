fc0041d5a8bb96239ebdb5d1dd11e8f6d9fec655
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-12-01 21:08:14 +0700

    Fix extra call mobile

--- src/app/(frontend)/_components/CategoryPage/mobile/InfiniteProductLoaderMobile.tsx
+++ src/app/(frontend)/_components/CategoryPage/mobile/InfiniteProductLoaderMobile.tsx
@@ -43,7 +43,7 @@ export default function InfiniteProductLoaderMobile({
       return nextPageNum <= lastPage.pagination.total_pages ? nextPageNum : undefined;
     },
     initialPageParam: currentPage + 1,
-    enabled: true,
+    enabled: false,
     staleTime: 5 * 60 * 1000, // 5 minutes
   });
 
