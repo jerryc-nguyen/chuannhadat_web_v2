9af8468ef9243f0bc224a48c8897797354bc98c6
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-12-13 22:54:42 +0700

    Remove hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { cn } from '@common/utils';
 import type { Metadata, Viewport } from 'next';
 import { Be_Vietnam_Pro } from 'next/font/google';
 import Head from 'next/head';
-import Script from 'next/script';
 import NextTopLoaderDynamic from '@/components/NextTopLoaderDynamic';
 import './index.scss';
 import ProviderWrapper from './provider-wrapper';
@@ -160,19 +159,6 @@ export default async function RootLayout({
       >
         <NextTopLoaderDynamic />
         <ProviderWrapper isMobile={isMobile}>{children}</ProviderWrapper>
-        {/* Hotjar script - deferred for PageSpeed */}
-        <Script id="hotjar" strategy="lazyOnload">
-          {`
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:6375646,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </Script>
       </body>
     </html>
   );
