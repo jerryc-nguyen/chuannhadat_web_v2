36cb34c92e986cef22986a8d38d1f255f619cfca
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2025-12-13 23:25:29 +0700

    Show media counter

---
+++ src/app/(frontend)/_components/CategoryPage/components/ThumbsCarousel/MediaCount.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ImageIcon } from 'lucide-react';
+import { cn } from '@common/utils';
+
+type Props = {
+  count?: number;
+  className?: string;
+};
+
+const MediaCount: React.FC<Props> = ({ count = 0, className }) => {
+  if (!count || count <= 0) return null;
+  return (
+    <div
+      className={cn(
+        'absolute right-2 bottom-2 flex items-center gap-1 rounded-full px-3 py-1 text-white bg-black/30',
+        className,
+      )}
+    >
+      <ImageIcon size={18} />
+      <span>{count}</span>
+    </div>
+  );
+};
+
+export default MediaCount;
